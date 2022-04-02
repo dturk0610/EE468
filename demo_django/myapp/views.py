48ba67475d33c4f4efe42f7b9ce3b1af622aabbe
Morgan Busch <buschmd@clarkson.edu>
2022-04-02 13:34:18 -0400

    Playing around with views

--- demo_django/myapp/views.py
+++ demo_django/myapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Department
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+
+def allDepartment(request):
+    dept_info = ""
+    for dept in Department.objects.all():
+        dept_info += f'<p>{dept}</p>'
+    html = "<html><h1>Test page.</h1><h2>All Department info:</h2>" + dept_info + "</html>"
+    
+    return HttpResponse(html)
