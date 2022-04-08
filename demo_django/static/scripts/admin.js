8b2b2e0f7547fe7a371a79279f27567d34fb47ef
Dakota Turk <dturk0610@gmail.com>
2022-04-08 11:56:18 -0400

    reimplementing f1

--- demo_django/static/scripts/admin.js
+++ demo_django/static/scripts/admin.js
@@ -1,3 +1,11 @@
-function tryClick(){
-    location.assign('/myapp/api/F1');
+function listAllProf(){
+    console.log("YOYOYO");
+    const Http = new XMLHttpRequest();
+    const url = '/myapp/api/F1';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange=(e)=>{
+        document.getElementById("resDiv").innerHTML+=Http.response;
+    }
 }
